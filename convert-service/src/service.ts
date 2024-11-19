2f62e494c9edfdb603d60d800846f8947332196e
Franklin Koch <franklinwkoch@gmail.com>
2024-11-18 21:26:31 -0700

    🔧 Fixes and logging to cloudrun service

--- convert-service/src/service.ts
+++ convert-service/src/service.ts
@@ -11,14 +11,27 @@ import { jatsConvert } from 'jats-convert';
 import { build, init, Session } from 'myst-cli';
 import { ProjectConfig, SiteConfig } from 'myst-config';
 import { Download } from 'myst-frontmatter';
-import { getFiles, removeTempFolder, respondBadRequest } from './utils.js';
+import {
+  getFiles,
+  logStatus,
+  removeFolder,
+  respondBadRequest,
+} from './utils.js';
 
 const PROJECT = 'curvenote-dev-1';
 const BUCKET = 'pmc-jats-curvenote-dev-1';
-const FOLDER = 'convert-service-test-1';
+const FOLDER = 'convert-service-test-0';
 
 const LISTING = '/usr/app/listing.csv';
 const TEMPLATE = '/usr/app/theme';
+
+// Time steps for logging, based on some averages
+const TS0 = 1; // start
+const TS1 = TS0 + 6; // download done
+const TS2 = TS1 + 2; // convert done
+const TS3 = TS2 + 10; // build done
+const TS4 = TS3 + 4; // upload done
+const TS5 = TS4 + 1; // done
 
 export function createService() {
   const app = express();
@@ -49,16 +62,22 @@ export function createService() {
     if (files.length > 0) {
       return res.status(204).send(`ID already processed: ${id}`);
     }
-    // Log downloading
+    await logStatus(
+      bucket,
+      prefix,
+      'processing',
+      `Downloading data for ${id}`,
+      TS0 / TS5
+    );
     let tmpFolder: string | undefined;
     let cwd: string | undefined;
     try {
       cwd = process.cwd();
       tmpFolder = fs.mkdtempSync(path.join(os.tmpdir(), 'jats'));
-      if (fs.existsSync(tmpFolder)) fs.rmSync(tmpFolder, { recursive: true });
+      removeFolder(tmpFolder);
       fs.mkdirSync(tmpFolder, { recursive: true });
-      console.info(`Made temp folder at: ${tmpFolder}`);
       process.chdir(tmpFolder);
+      console.info(`Working in temp folder: ${tmpFolder}`);
       const start = Date.now() / 1000;
       const session = new Session();
       const jatsFile = `${tmpFolder}/${id}.xml`;
@@ -67,14 +86,26 @@ export function createService() {
         data: true,
         listing: LISTING,
       });
-      // Log converting
+      await logStatus(
+        bucket,
+        prefix,
+        'processing',
+        `Converting data to MyST for ${id}`,
+        TS1 / TS5
+      );
       const downloadDone = Date.now() / 1000;
-      await jatsConvert(jatsFile, {
+      await jatsConvert(session, jatsFile, {
         frontmatter: 'project',
         dois: false,
         bibtex: true,
       });
-      // Log building
+      await logStatus(
+        bucket,
+        prefix,
+        'processing',
+        `Building MyST site for ${id}`,
+        TS2 / TS5
+      );
       await init(session, { project: true, site: true });
       const mystFile = 'myst.yml';
       const mystYml = yaml.load(fs.readFileSync(mystFile).toString()) as {
@@ -92,9 +123,16 @@ export function createService() {
       ];
       mystYml.site.template = TEMPLATE;
       fs.writeFileSync(mystFile, yaml.dump(mystYml));
+      await session.reload();
       await build(session, [], { site: true });
       const processDone = Date.now() / 1000;
-      // Log saving
+      await logStatus(
+        bucket,
+        prefix,
+        'processing',
+        `Saving data for ${id}`,
+        TS3 / TS5
+      );
       for await (const filePath of getFiles('_build/site')) {
         try {
           const destination = `${prefix}/${path.relative(
@@ -104,6 +142,13 @@ export function createService() {
           await bucket.upload(filePath, { destination });
         } catch {}
       }
+      await logStatus(
+        bucket,
+        prefix,
+        'processing',
+        `Finalizing processing for ${id}`,
+        TS4 / TS5
+      );
       const uploadDone = Date.now() / 1000;
       const logFile = `${id}.log.yml`;
       const logData = yaml.load(fs.readFileSync(logFile).toString()) as Record<
@@ -124,15 +169,26 @@ export function createService() {
       fs.writeFileSync(logFile, yaml.dump(logData));
       await bucket.upload(logFile, { destination: `${prefix}/${logFile}` });
       process.chdir(cwd);
-      removeTempFolder(tmpFolder);
-      // Log success
+      removeFolder(tmpFolder);
+      await logStatus(
+        bucket,
+        prefix,
+        'success',
+        `Completed processing ${id}`,
+        1
+      );
       return res.sendStatus(204);
     } catch (err: any) {
-      // Log failure
-      console.error(err, `JATS convert job failed for ID: ${id}`);
+      try {
+        await logStatus(bucket, prefix, 'failure', `Unable to process ${id}`);
+      } catch {
+        // Carry on if logging the failure errors.
+      }
+      console.error(err);
+      console.error(`JATS convert job failed for ID: ${id}`);
       if (cwd) process.chdir(cwd);
-      if (tmpFolder) removeTempFolder(tmpFolder);
-      return res.status(204).send(err.message);
+      removeFolder(tmpFolder);
+      return res.status(204).send(`Unable to process ${id}`);
     }
   });
 
--- convert-service/src/utils.ts
+++ convert-service/src/utils.ts
@@ -10,19 +10,18 @@ export function respondBadRequest(res: Response, msg: string) {
   return res.status(400).send(`Bad Request: ${msg}`);
 }
 
-export function removeTempFolder(tempFolder: string) {
-  if (tempFolder && fs.existsSync(tempFolder)) {
-    fs.rmSync(tempFolder, { recursive: true });
+export function removeFolder(folder?: string) {
+  if (folder && fs.existsSync(folder)) {
+    fs.rmSync(folder, { recursive: true });
   }
-}
-
-export function createTempFolder() {
-  return fs.mkdtempSync(path.join(os.tmpdir(), 'export'));
 }
 
 const readdir = util.promisify(fs.readdir);
 const stat = util.promisify(fs.stat);
 
+/**
+ * From google cloud docs for uploading a directory to a bucket
+ */
 export async function* getFiles(directory = '.'): AsyncGenerator<string> {
   for (const file of await readdir(directory)) {
     const fullPath = path.join(directory, file);
@@ -46,9 +45,11 @@ export async function logStatus(
   bucket: Bucket,
   prefix: string,
   status: 'processing' | 'success' | 'failure',
-  message?: string
+  message?: string,
+  progress?: number
 ) {
+  if (message) console.info(message);
   const logFile = `${prefix}/status.json`;
-  const logEntry = { timestamp: +Date.now(), status, message };
+  const logEntry = { timestamp: +Date.now(), status, message, progress };
   await bucket.file(logFile).save(JSON.stringify(logEntry));
 }
