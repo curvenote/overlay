789fa3ebd975bed94cf90e79b89357a96371974c
Rowan Cockett <rowanc1@gmail.com>
2024-11-27 16:14:39 -0700

    ⭕️ Refactor button

--- theme/app/components/GitHubIssueButton.tsx
+++ theme/app/components/GitHubIssueButton.tsx
@@ -1,41 +1,27 @@
 import React from 'react';
+import { Tooltip } from 'myst-to-react';
+import { ShieldAlert } from 'lucide-react';
 
 const GitHubIssueButton: React.FC<{ id: string }> = ({ id }) => {
-  const openGitHubIssue = () => {
-    const repoOwner = 'curvenote';
-    const repoName = 'jats-xml';
-    const issueTitle = encodeURIComponent(`${id}: `);
-    const issueBody = encodeURIComponent(
-      `<!-- Describe the PMC content issue -->\n\n<!-- Screenshots are nice, but no need to link back to the article -->\n\nID: ${id}`,
-    );
-
-    const url = `https://github.com/${repoOwner}/${repoName}/issues/new?title=${issueTitle}&body=${issueBody}`;
-    window.open(url, '_blank');
-  };
+  const repoOwner = 'curvenote';
+  const repoName = 'jats-xml';
+  const issueTitle = encodeURIComponent(`${id}: `);
+  const issueBody = encodeURIComponent(
+    `<!-- Describe the PMC content issue -->\n\n<!-- Screenshots are nice, but no need to link back to the article -->\n\nID: ${id}`,
+  );
+  const url = `https://github.com/${repoOwner}/${repoName}/issues/new?title=${issueTitle}&body=${issueBody}`;
 
   return (
-    <button style={styles.button} onClick={openGitHubIssue}>
-      Report an Issue
-    </button>
+    <Tooltip title="Report an Issue">
+      <a
+        href={url}
+        target="_blank"
+        className="fixed bottom-2 right-2 p-2 z-10 bg-[#F3F4F5] border hover:border-white hover:shadow-lg hover:bg-[#DF4F4A] text-black hover:text-white rounded shadow no-underline text-base font-normal"
+      >
+        <ShieldAlert size={20} />
+      </a>
+    </Tooltip>
   );
-};
-
-const styles: { [key: string]: React.CSSProperties } = {
-  button: {
-    position: 'fixed',
-    top: '10px',
-    right: '10px',
-    zIndex: 1000,
-    backgroundColor: '#345E98',
-    color: '#fff',
-    border: 'none',
-    borderRadius: '4px',
-    padding: '10px 20px',
-    fontSize: '14px',
-    cursor: 'pointer',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
-    transition: 'background-color 0.3s ease',
-  },
 };
 
 export default GitHubIssueButton;
